32509cd83a1d96ffe6afb9a3d6def22c8a63c660
Eugen Beck <ebeck@apptek.com>
2024-10-09 12:11:49 +0000

    Revert "Rename module"
    This reverts commit b6cad99a8d3d2066785d80fa6ff15afafc47768a.

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
             include_dirs=[FAST_VITERBI_DIR, COMMON_DIR],
         ),
         CUDAExtension(
-            name="i6_native_ops.monotonic_rnnt.core",
+            name="i6_native_ops.monotonic_rnnt.monotonic_rnnt_core",
             sources=[f"{MONOTONIC_RNNT_DIR}/monotonic_rnnt.cu"],
             include_dirs=[f"{MONOTONIC_RNNT_DIR}/include"],
             extra_compile_args=["-DRNNT_ENABLE_GPU"],
