aa65947b82bd52f0de619e1cd8a8000cfcbb6839
Benedikt Conze <benedikt.conze@rwth-aachen.de>
2024-11-04 19:56:44 +0100

    Add check for tensor element type

--- i6_native_ops/fbw/fbw_torch.cpp
+++ i6_native_ops/fbw/fbw_torch.cpp
@@ -13,19 +13,21 @@ std::vector<torch::Tensor> fbw_cuda(torch::Tensor& am_scores, torch::Tensor& edg
 
 #define CHECK_CUDA(x) TORCH_CHECK(x.device().is_cuda(), #x " must be a CUDA tensor")
 #define CHECK_CONTIGUOUS(x) TORCH_CHECK(x.is_contiguous(), #x " must be contiguous")
-#define CHECK_INPUT(x) \
-    CHECK_CUDA(x);     \
-    CHECK_CONTIGUOUS(x)
+#define CHECK_TYPE(x, type) TORCH_CHECK(x.dtype() == type, "Expected " #x " to contain elements of type ", type, ", but got type ", x.dtype())
+#define CHECK_INPUT(x, type) \
+    CHECK_CUDA(x);           \
+    CHECK_CONTIGUOUS(x);     \
+    CHECK_TYPE(x, type)
 
 std::vector<torch::Tensor> fbw(torch::Tensor& am_scores, torch::Tensor& edges,
                                torch::Tensor& weights, torch::Tensor& start_end_states,
                                torch::Tensor& seq_lens, unsigned num_states,
                                DebugOptions debug_options = DebugOptions()) {
-    CHECK_INPUT(am_scores);
-    CHECK_INPUT(edges);
-    CHECK_INPUT(weights);
-    CHECK_INPUT(start_end_states);
-    CHECK_INPUT(seq_lens);
+    CHECK_INPUT(am_scores, torch::kFloat32);
+    CHECK_INPUT(edges, torch::kInt32);
+    CHECK_INPUT(weights, torch::kFloat32);
+    CHECK_INPUT(start_end_states, torch::kInt32);
+    CHECK_INPUT(seq_lens, torch::kInt32);
 
     auto outputs = fbw_cuda(am_scores, edges, weights, start_end_states, seq_lens, num_states,
                             debug_options);
